83c3fe46a7a21a28b2e1a9b8800b558585621630
Jonathan Wight <jwight@mac.com>
2025-02-10 11:14:37 -0800

    Improve spzcli to convert ply -> spz... Add samples.

--- Sources/spzcli/spzcli.swift
+++ Sources/spzcli/spzcli.swift
@@ -1,16 +1,33 @@
 import spz_cpp
+import Foundation
 
 @main
 struct SPZCLI {
 
     static func main() {
-        print("Hello world")
 
-        let cloud = spz.loadSplatFromPly("/Users/schwa/Downloads/321_baseline_modified.ply")
-        print(cloud)
+        guard CommandLine.arguments.count == 3 else {
+            print("Usage: spzcli INPUT OUTPUT")
+            return
+        }
 
-        print(cloud.numPoints)
+        let inputPath = URL(fileURLWithPath: CommandLine.arguments[1])
+        let outputPath = URL(fileURLWithPath: CommandLine.arguments[2])
 
-        spz.saveSpz(cloud, "test.spz")
+        print(inputPath.pathExtension, outputPath.pathExtension)
+
+        switch (inputPath.pathExtension, outputPath.pathExtension) {
+        case ("ply", "spz"):
+            let cloud = spz.loadSplatFromPly(std.string(inputPath.path))
+            spz.saveSpz(cloud, std.string(outputPath.path))
+
+            let loaded = spz.loadSpz(std.string(outputPath.path))
+            print(loaded)
+
+
+        default:
+            print("Unsupported file format \(inputPath.pathExtension) -> \(outputPath.pathExtension)")
+            return
+        }
     }
 }
